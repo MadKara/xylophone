1145d3351ee1a280a7fdb6ade36c6e0a49553928
Mariia Karalash <>
2021-04-19 21:35:17 +0300

    Achieved the effect of an animated button press

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundname: sender.currentTitle!)
+        
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2){
+            sender.alpha = 1
+        }
+    }
+    
+    func playSound(soundname: String) {
+        
+        let url = Bundle.main.url(forResource: soundname, withExtension: "wav")
+        
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 
 }
 
